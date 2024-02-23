a971a0469bc45aee207ea9304f25459d55125bf7
shuvo <143803395+shuvo2k0@users.noreply.github.com>
2024-02-23 17:22:51 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld 
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter two number : "+a+" "+b);
+            
+            Console.WriteLine("{0}+{1}={2}",a,b,a+b);
+            Console.WriteLine("{0}-{1}={2}",a,b,a-b);
+            Console.WriteLine("{0}*{1}={2}",a,b,a*b);
+            Console.WriteLine("{0}/{1}={2}",a,b,a/b);
+	        
+          
+            
+        }
+    }
+}
